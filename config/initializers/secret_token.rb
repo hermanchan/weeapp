34232631255200c91ed3befc38b6bedb8220f12b
Herman Chan <herman.chan@goprezzo.com>
2013-04-11 17:02:44 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hermanapp::Application.config.secret_token = '47e65d9ab12b9f4dd8759f3ec97206a544af77bce6e52bbe59eb1274a9b8085bb97b28e260b6f56da74591147535657dade5526cb118a0a9414b544e404f41e3'
